13018fa436873e24cdeab77e09ed76066903f2e9
lnowak <nowak.lukasz.1291@gmail.com>
2020-11-11 16:06:18 +0100

    Update QuestionArea.js
    Encoder finished

--- src/Components/QuestionArea.js
+++ src/Components/QuestionArea.js
@@ -1,10 +1,9 @@
 const QuestionArea = ({que, index, pageNo, setPageNo}) => {
 
     const encode = mystring => {
-        return mystring.replace(/&quot;/g, '"').replace(/&ldquo;/g, '"').replace(/&rdquo;/g, '"').replace(/&#039;/g, "'").replace(/&rsquo;/g, "'").replace(/&lsquo;/g, "'").replace(/&amp;/g, "&").replace(/&‌pi/g, "π").replace(/&shy;/g, "");
+        return mystring.replace(/&ouml;/g, 'o').replace(/&auml;/g, 'a').replace(/&quot;/g, '"').replace(/&ldquo;/g, '"').replace(/&rdquo;/g, '"').replace(/&euml;/g, "e").replace(/&hellip;/g, '...').replace(/&#039;/g, "'").replace(/&rsquo;/g, "'").replace(/&lsquo;/g, "'").replace(/&amp;/g, "&").replace(/&‌pi/g, "π").replace(/&shy;/g, "");
     }
     let question = encode(que.question);
-    console.log(question);
 
     const answers = [que.correct_answer];
     que.incorrect_answers.map(e => {
@@ -27,9 +26,11 @@ const QuestionArea = ({que, index, pageNo, setPageNo}) => {
             <p className='questionsArea__question'>{question}</p>
             <div className='questionsArea__answers'>
                 {answers.map(e => {
-                    encode(e)
-                    return <button key={e} className='questionsArea__answers__item' onClick={choseAnswer}>{e}</button>
+                    const answer = encode(e)
+                    return <button key={e} className='questionsArea__answers__item' onClick={choseAnswer}>{answer}</button>
                 })}
+                <button style={{height: '25px', width: '50px'}} onClick={() => setPageNo(() => pageNo + 1)}>+</button>
+                <button style={{height: '25px', width: '50px'}} onClick={() => setPageNo(() => pageNo - 1)} >-</button>
             </div>
         </div>
     )
