4260f2afbf5a370d725a59cd1d74d0bdb87085a1
lnowak <nowak.lukasz.1291@gmail.com>
2020-11-12 18:47:17 +0100

    Visual changes
    - Media queries added
    - New background added

--- src/Components/WelcomeScreen.js
+++ src/Components/WelcomeScreen.js
@@ -4,9 +4,9 @@ const WelcomeScreen = ({ isScreenActive, setIsScreenActive}) => {
     return (
         <div className={isScreenActive === 1 ? 'welcomeScreen' : 'hidden'} >
             <div className='screen__name welcomeScreen__name'>
-                <svg viewBox="0 0 500 500">
+                <svg viewBox="0 0 500 500" className='name'>
                     <path id="curve" d="M72.2,148.6c4-6.1,65.5-6.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
-                    <text className='lkl' width="500">
+                    <text>
                         <textPath href="#curve">
                             Quiz Game
                         </textPath>
