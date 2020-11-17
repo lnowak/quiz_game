ba3ffa64417728a6aa9ae92360449d06c2c6e9a5
lnowak <nowak.lukasz.1291@gmail.com>
2020-11-17 15:01:15 +0100

    Mediaqueries updated

--- src/Components/ChoiceScreen.js
+++ src/Components/ChoiceScreen.js
@@ -21,7 +21,7 @@ const ChoiceScreen = ({categories, setChosenCategory, chosenCategory, isScreenAc
 
     return (
         <div className={isScreenActive === 2 ? 'choiceScreen' : 'hidden'}>
-            <div className='screen__name'>
+            {/* <div className='screen__name'>
                 <svg viewBox="0 0 500 500">
                     <path id="curve" d="M72.2,148.6c4-6.1,65.5-6.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
                     <text className='lkl' width="500">
@@ -30,7 +30,7 @@ const ChoiceScreen = ({categories, setChosenCategory, chosenCategory, isScreenAc
                         </textPath>
                     </text>
                 </svg>
-            </div>
+            </div> */}
             <div className='logo'></div>
             <p className={categories ? 'categoryPick' : 'hidden'}>Select a category</p>
             <div className={categories ? 'choiceScreen__chooseMenu' : 'hidden'}>
